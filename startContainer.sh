d84c6d0b03cb7fbbd58e444b8e2df45a4a1f44ee
Julio Amador <julioadl@gmail.com>
2018-11-27 14:11:41 +0000

    added generate-config for jupyter

--- startContainer.sh
+++ startContainer.sh
@@ -1,3 +1,6 @@
 python3.6 python -m nltk.downloader all \
 export LC_ALL=C.UTF-8 \
-export LANG=C.UTF-8
+export LANG=C.UTF-8 \
+git config --global user.email "julioadl@gmail.com" \
+git config --global user.name "Julio" \
+jupyter notebook --generate-config
